be9fedb286a160b22242f50a9228fd53439c0aca
Sagie Maoz <sagie@maoz.info>
2012-01-06 14:57:51 +0200

    first commit

---
+++ src/BinomialHeap.java
@@ -0,0 +1,126 @@
+/**
+ * BinomialHeap
+ *
+ * An implementation of binomial heap over non-negative integers.
+ */
+public class BinomialHeap
+{
+
+   /**
+    * public boolean empty()
+    *
+    * precondition: none
+    * 
+    * The method returns true if and only if the heap
+    * is empty.
+    *   
+    */
+    public boolean empty()
+    {
+    	return false; // should be replaced by student code
+    }
+		
+   /**
+    * public void insert(int value)
+    *
+    * Insert value into the heap 
+    *
+    */
+    public void insert(int value) 
+    {    
+    	return; // should be replaced by student code
+    }
+
+   /**
+    * public void deleteMin()
+    *
+    * Delete the minimum value
+    *
+    */
+    public void deleteMin()
+    {
+     	return; // should be replaced by student code
+     	
+    }
+
+   /**
+    * public int findMin()
+    *
+    * Return the minimum value
+    *
+    */
+    public int findMin()
+    {
+    	return 42;// should be replaced by student code
+    } 
+    
+   /**
+    * public void meld (BinomialHeap heap2)
+    *
+    * Meld the heap with heap2
+    *
+    */
+    public void meld (BinomialHeap heap2)
+    {
+    	  return; // should be replaced by student code   		
+    }
+
+   /**
+    * public int size()
+    *
+    * Return the number of elements in the heap
+    *   
+    */
+    public int size()
+    {
+    	return 42; // should be replaced by student code
+    }
+    
+   /**
+    * public int[] treesSize()
+    *
+    * Return an array containing the sizes of the trees that represent the heap
+    * in ascending order.
+    * 
+    */
+    public int[] treesSize()
+    {
+        int[] arr = new int[42]; //
+        return arr; //	 to be replaced by student code
+    }
+
+   /**
+    * public void arrayToHeap()
+    *
+    * Insert the array to the heap. Delete previous elemnts in the heap.
+    * 
+    */
+    public void arrayToHeap(int[] array)
+    {
+        return; //	 to be replaced by student code
+    }
+	
+   /**
+    * public boolean isHeap()
+    *
+    * Returns true if and only if the heap is valid.
+    *   
+    */
+    public boolean isHeap() 
+    {
+    	return false; // should be replaced by student code
+    }
+    
+   /**
+    * public class HeapNode
+    * 
+    * If you wish to implement classes other than BinomialHeap
+    * (for example HeapNode), do it in this file, not in 
+    * another file 
+    *  
+    */
+    public class HeapNode{
+  	
+    }
+
+}
